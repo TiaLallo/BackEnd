f97f3e1dedbed0240b314ce13c11cf7a8c322abd
TiaLallo <tia.lallo@gmail.com>
2019-01-28 16:40:36 +0200

    PersonController and PersonRepository updated

--- WebApiExample/WebApiExample/Startup.cs
+++ WebApiExample/WebApiExample/Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WebApiExample.Models;
+using WebApiExample.Repositories;
 
 namespace WebApiExample
 {
@@ -25,6 +28,12 @@ namespace WebApiExample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IPersonRepository, PersonRepository>();
+            services.AddDbContext<PersondbContext>(opt =>
+            opt.UseSqlServer(Configuration.GetConnectionString("LocalPersonDbContext")));
+
+            services.AddMvc().AddJsonOptions(json => json.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
